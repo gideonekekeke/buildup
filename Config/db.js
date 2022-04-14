47f45990e267d57b9bf51088038e030f7b45ed21
gideonekekeke <gideonekeke64@gmail.com>
2022-04-14 08:13:51 +0100

    done

---
+++ Config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+const url = "mongodb+srv://admin:ilovemusic1234@cluster0.21gv0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+
+mongoose.connect(url)
+
+mongoose.connection
+.on("open", (data)=>{
+  console.log("database is conneted sucessfully");
+})
+.once("error", (strem)=>{
+console.log("database cant conect");
+})
+
+module.exports = mongoose
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+require('dotenv').config()
+require("./config/db.js")
 const express = require('express')
 const port = process.env.PORT || 5030
 
-
+const cors = require("cors")
 const app = express()
 app.use(express.json())
+app.use(cors())
+
+const myRouter = require("./Router/router")
+const myRouter2 = require("./Router/router2")
+
+app.use("/api", require("./Router/router2"))
+app.use("/api", myRouter)
 
 
 app.get('/', (req, res)=>{
